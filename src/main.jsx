c16a1d533a7662b9382506eaa73098cccf402780
HoaiPC <machhoai1234@gmail.com>
2025-04-01 22:50:36 +0700

    fix: correct import case for App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './styles/index.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import App from './app.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
